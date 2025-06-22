789d04ab645e9ef75f2fb468e6736faea3347aca
Amarjeet <amarjeetgiri020@gmail.com>
2025-06-23 01:48:56 +0530

    Work going on

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import BlogList from '../components/BlogList'
+import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
     <>
       <Navbar/>
+      <Header/>
+      <BlogList/>
     </>
   )
 }
