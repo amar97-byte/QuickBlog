557f833d2f601208bacdd280e80a2eb0d998d605
Amarjeet <amarjeetgiri020@gmail.com>
2025-07-03 02:28:45 +0530

    connection with api's has been stablished

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import 'dotenv/config'
 import cors from "cors"
+import connectDB from "./config/db.js"
+import adminRouter from "./routes/admin.route.js"
 
 const app = express()
 
@@ -10,9 +12,16 @@ app.use(cors())
 app.use(express.json())
 
 
+await connectDB()
+
+// ROUTES
 app.get("/" , (req,res)=> {
     res.send("Api is working")
 })
+
+app.use("/api/admin" , adminRouter)
+
+
 
 const PORT = 3000 || process.env.PORT
 
