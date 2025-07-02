716d2e9da94a372c4c2468734a9456b060c30a88
Amarjeet <amarjeetgiri020@gmail.com>
2025-07-03 02:27:56 +0530

    Blog model has been created

---
+++ server/models/blog.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+
+const blogSchema = new mongoose.Schema({
+    title : {
+        type : String ,
+        requird : true
+    },
+    subTitle : {
+        type : String 
+    },
+    description : {
+        type : String ,
+        required : true
+    },
+    image : {
+        type : String ,
+        required : true
+    },
+    isPublished : {
+        type : Boolean ,
+        required : true
+    },
+} ,{timestamps : true})
+
+const Blog = mongoose.model("blog" , blogSchema)
+
+export default Blog
