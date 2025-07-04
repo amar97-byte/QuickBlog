ae8284967ced5a3d51979c47e1e7794e64d0261b
Amarjeet <amarjeetgiri020@gmail.com>
2025-07-05 02:39:16 +0530

    more routes for Blog model is added

--- server/routes/blog.route.js
+++ server/routes/blog.route.js
@@ -1,10 +1,15 @@
 import express from "express"
-import { addBlog } from "../controllers/blog.controller.js"
+import { addBlog, deleteBlog, getBlogById, getBlogList, togglePublish } from "../controllers/blog.controller.js"
 import upload from "../middleware/multer.middleware.js"
 import auth from "../middleware/auth.middleware.js"
 
 const blogRouter = express.Router()
 
 blogRouter.post("/add"  , upload.single('image') , auth ,addBlog)
+blogRouter.get("/blogList" , getBlogList)
+blogRouter.get("/:blogId" , getBlogById)
+blogRouter.post("/delete" ,auth, deleteBlog)
+blogRouter.post("/toggle-publish" ,auth, togglePublish)
+
 
 export default blogRouter
