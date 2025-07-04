ad7ccd5d33c68051efa482de5edf9ff31a849d10
Amarjeet <amarjeetgiri020@gmail.com>
2025-07-05 02:40:24 +0530

    Blog controllres for different API's call are in progress

--- server/controllers/blog.controller.js
+++ server/controllers/blog.controller.js
@@ -54,3 +54,86 @@ export const addBlog = async(req,res)=> {
         
     }
 }
+
+
+// GET ALL BLOGS
+
+ export const  getBlogList = async(req , res)=>{
+    try {
+        const blogs = await Blog.find({isPublished : true})
+        res.json({success : true , blogs})
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success : false , message : error.message})
+
+        
+    }
+}
+
+
+// INDIVIDUAL BLOG DATA
+export const getBlogById = async (req , res)=> {
+    try {
+        const {blogId} = req.params
+        const blog = await Blog.findById(blogId)
+
+        if(!blog){
+            return res.json({success : false , message : "Blog not found"})
+        }
+
+        res.json({success : true , blog})
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success : false , message : error.message})
+    }
+}
+
+
+// DELETE ANY BLOG
+
+export const deleteBlog = async (req , res)=> {
+    try {
+        const {id} = req.body
+        const blog = await Blog.findByIdAndDelete(id)
+
+        res.json({success : true , message : "Blog deleted Successfully"})
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success : false , message : error.message})
+    }
+}
+
+
+//   CHANGING THE BLOG STATUS
+export const togglePublish = async (req , res)=>{
+    try {
+        const{id} = req.body
+        const blog = await Blog.findById(id)
+        blog.isPublished = !blog.isPublished
+
+        await blog.save()
+        res.json({success : true , message : "Blog status updated Successfully"})
+
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success : false , message : error.message})
+    }
+}
+
+
+// ADD COMMENT
+
+export const addComment = async (req , res)=>{
+    try {
+        const {blog , name , content} = req.body
+     
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.json({success : false , message : error.message})
+    }
+}
+
