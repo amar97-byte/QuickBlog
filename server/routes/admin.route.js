4a3b79d20601a44dc1992b6572477c4d9acd5261
Amarjeet <amarjeetgiri020@gmail.com>
2025-07-06 00:29:04 +0530

    Routes for the Admin controllers are done

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,8 +1,14 @@
 import express from "express"
-import { adminLogin } from "../controllers/admin.controller.js"
+import { adminLogin, approveCommentById, deleteCommentById, getAllBlogAdmin, getAllComments, getDashboard } from "../controllers/admin.controller.js"
+import auth from "../middleware/auth.middleware.js"
 
 const adminRouter = express.Router()
 
 adminRouter.post("/login" , adminLogin)
+adminRouter.get("/comments" ,auth , getAllComments)
+adminRouter.get("/blogs" ,auth , getAllBlogAdmin)
+adminRouter.post("/delete-comment" ,auth , deleteCommentById)
+adminRouter.post("/approve-comment" ,auth , approveCommentById)
+adminRouter.get("/dashboard" ,auth , getDashboard)
 
 export default adminRouter
