e3c014be1ed95ff43d3343e39bfd8d64de6d7c2e
Amarjeet <amarjeetgiri020@gmail.com>
2025-07-06 00:28:26 +0530

    Controllers fot admin are made

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -1,20 +1,94 @@
-import express from "express"
-import jwt from "jsonwebtoken"
+import express from "express";
+import jwt from "jsonwebtoken";
+import Blog from "../models/blog.model.js";
+import Comment from "../models/comment.model.js";
 
 // ADMIN LOGIN FUNCTION
-export const adminLogin = async (req,res)=> {
-try {
-    const {email , password} = req.body
+export const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-    if(email != process.env.ADMIN_EMAIL || password != process.env.ADMIN_PASSWORD ){
-        return res.json({success : false , message : "Incorrect Credentials"})
+    if (
+      email != process.env.ADMIN_EMAIL ||
+      password != process.env.ADMIN_PASSWORD
+    ) {
+      return res.json({ success: false, message: "Incorrect Credentials" });
     }
 
-    const token = jwt.sign({email} , process.env.JWT_SECRET_KEY)
-    res.json({success : true , token})
-} catch (error) {
+    const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY);
+    res.json({ success: true, token });
+  } catch (error) {
     console.log(error.message);
-    res.json({success : false , message : error.message})
-    
-}
-}
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// GET ALL THE BLOGS FOR ADMIN
+export const getAllBlogAdmin = async (req, re) => {
+  try {
+    const blogs = await Blog.find({}).sort({ createdAt: -1 });
+
+    res.json({ success: true, blogs });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//  GET ALL THE COMMENTS FOR ADMIN
+export const getAllComments = async (req, re) => {
+  try {
+    const comments = await Comment.find({})
+      .populate("blog")
+      .sort({ createdAt: -1 });
+    // populate() method bring the realted blogs also that aare associated with comments
+
+    res.json({ success: true, comments });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//  GET ALL THE DASHBOARD DATA FOR ADMIN
+export const getDashboard = async (req, re) => {
+  try {
+    const recentBlogs = await Blog.find({}).sort({ createdAt: -1 }).limit(5);
+    const blogs = await Blog.countDocuments();
+    const comments = await Comment.countDocuments();
+    const drafts = await Blog.countDocuments({ isPublished: false });
+
+    const dashboardData = { blogs, comments, drafts, recentBlogs };
+
+    res.json({ success: true, dashboardData });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// TO DELETE COMMENT BY ADMIN
+export const deleteCommentById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await Comment.findByIdAndDelete(id);
+
+    res.json({ success: true, message: "Comment Deleted Successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// TO APPROVE THE COMMENT BY ADMIN
+export const approveCommentById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await Comment.findByIdAndUpdate(id, { isApproved: true });
+
+    res.json({ success: true, message: "Comment Approved Successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
