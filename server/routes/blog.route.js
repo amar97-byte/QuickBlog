6fe7d6712d68cc4ddec4b514e610386a9a073858
Amarjeet <amarjeetgiri020@gmail.com>
2025-07-07 03:32:44 +0530

    Routes for the QuickBlog backend is done

--- server/routes/blog.route.js
+++ server/routes/blog.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addBlog, addComment, deleteBlog, getBlogById, getBlogComments, getBlogList, togglePublish } from "../controllers/blog.controller.js"
+import { addBlog, addComment, deleteBlog, generateContent, getBlogById, getBlogComments, getBlogList, togglePublish } from "../controllers/blog.controller.js"
 import upload from "../middleware/multer.middleware.js"
 import auth from "../middleware/auth.middleware.js"
 
@@ -13,6 +13,9 @@ blogRouter.post("/toggle-publish" ,auth, togglePublish)
 
 blogRouter.post("/addComment" , addComment)
 blogRouter.post("/comments" , getBlogComments)
+
+// GENERATING CONTENT USING THE GEMINI AI
+blogRouter.post("/generate-content" ,auth , generateContent)
 
 
 export default blogRouter
