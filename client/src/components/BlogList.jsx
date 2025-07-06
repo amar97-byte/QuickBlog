59ee12871720281385805a91934b452fa31f6dbc
Amarjeet <amarjeetgiri020@gmail.com>
2025-07-07 00:51:49 +0530

    Connected with thw backend API

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -2,9 +2,22 @@ import  { useState } from "react";
 import { blog_data, blogCategories } from "../assets/assets";
 import {motion} from "motion/react"
 import BlogCard from "./BlogCard";
+import { useAppContext } from "../Context/AppContext";
 
 const BlogList = () => {
+
+  const {blogs , input} = useAppContext()
+  // console.log(blogs);
+  
   const [menu, setMenu] = useState("All");
+
+  const filterBlogs = ()=> {
+    if(input === ''){
+      return blogs
+    }
+    return blogs.filter((blog)=>blog.title.toLowerCase().includes(input.toLowerCase()) || 
+    blog.category.toLowerCase().includes(input.toLowerCase()))
+  }
 
   return (
     <div>
@@ -28,7 +41,7 @@ const BlogList = () => {
 
         {/* ------ blog cards ------ */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-25 mx-8 sm:mx-16 xl:mx-40">
-            {blog_data.filter((blog)=> menu === "All" ? true : blog.category === menu).
+            {filterBlogs().filter((blog)=> menu === "All" ? true : blog.category === menu).
             map((blog)=><BlogCard key={blog._id} blog={blog}/>)}
       </div>
     </div>
